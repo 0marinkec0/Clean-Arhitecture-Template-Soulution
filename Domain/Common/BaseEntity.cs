744305a751fedebdbb27b16efd5017e03bed28cc
Marinkec <vc.petric@gmail.com>
2022-07-03 12:54:46 +0200

    SecondCommit

--- Domain/Common/BaseEntity.cs
+++ Domain/Common/BaseEntity.cs
@@ -1,11 +1,10 @@
-﻿namespace Domain.Common
+﻿namespace Domain.Common;
+
+public class BaseEntity
 {
-    public class BaseEntity
-    {
-        public int Id { get; set; }
-        public int CreatedBy { get; set; }
-        public DateTime DateCreated { get; set; }
-        public int? ModifiedBy { get; set; }
-        public DateTime? DateModified { get; set; }
-    }
+    public int Id { get; set; }
+    public int CreatedBy { get; set; }
+    public DateTime DateCreated { get; set; }
+    public int? ModifiedBy { get; set; }
+    public DateTime? DateModified { get; set; }
 }
